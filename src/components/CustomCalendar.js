393fd4c6114af06fb9fdbfd7495cc5df57dd3b59
Vincent Desmouceaux <desmontvincent@gmail.com>
2023-06-05 09:39:56 +0200

    affichage calendrier

--- src/components/CustomCalendar.js
+++ src/components/CustomCalendar.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 const CustomCalendar = ({ value, onChange }) => {
+  const [showCalendar, setShowCalendar] = useState(false);
+
   const handleDateChange = (date) => {
     onChange(date);
   };
 
+  const handleInputFocus = () => {
+    setShowCalendar(true);
+  };
+
+  const handleInputBlur = () => {
+    setShowCalendar(false);
+  };
+
   return (
-    <Calendar
-      value={value}
-      onChange={handleDateChange}
-      selectRange={true}
-      formatLongDate={(locale, date) => date.toLocaleDateString()}
-    />
+    <div>
+      <input
+        type="text"
+        placeholder="Search date"
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+      />
+      {showCalendar && (
+        <Calendar
+          value={value}
+          onChange={handleDateChange}
+          selectRange={true}
+          formatLongDate={(locale, date) => date.toLocaleDateString()}
+        />
+      )}
+    </div>
   );
 };
 
